586ea4f5d2f9fac56ccb131a12c3082eebd5ed72
Wesley <wesleyadwebster@gmail.com>
2023-06-16 23:57:24 -0500

    Carousel and function set up. Testing functionality

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,35 @@ const carouselImageOne = 'assets/carousel-images/carousel-image-1.jpg';
 const carouselImageTwo = 'assets/carousel-images/carousel-image-2.jpg';
 const carouselImageThree = 'assets/carousel-images/carousel-image-3.jpg';
 
-console.log(carouselImageOne);
+const carouselImages = [carouselImageOne, carouselImageTwo, carouselImageThree];
+
+console.log(carouselImages);
+
+// Get necessary elements
+const carouselImgElement = document.getElementById("carousel-img");
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
+
+let currentImageIndex = 0;
+
+// Function to update the image and buttons
+function updateCarousel() {
+    carouselImgElement.src = carouselImages[currentImageIndex];
+
+    prevBtn.disabled = currentImageIndex === 0;
+    nextBtn.disabled = currentImageIndex === carouselImages.length - 1;
+}
+
+// Set initial image
+updateCarousel();
+
+// Set up event listeners for buttons
+prevBtn.addEventListener("click", function () {
+    currentImageIndex--;
+    updateCarousel();
+});
+
+nextBtn.addEventListener("click", function () {
+    currentImageIndex++;
+    updateCarousel();
+});
