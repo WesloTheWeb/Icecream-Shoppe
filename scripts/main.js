7255ef32a48cf7bae76394f2e3c8130e30188929
Wesley <wesleyadwebster@gmail.com>
2023-06-16 22:21:04 -0500

    Deleted remnants tailwind, added carousel images, sync to main js file

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+const carouselImageOne = 'assets/carousel-images/carousel-image-1.jpg';
+const carouselImageTwo = 'assets/carousel-images/carousel-image-2.jpg';
+const carouselImageThree = 'assets/carousel-images/carousel-image-3.jpg';
+
+console.log(carouselImageOne);
