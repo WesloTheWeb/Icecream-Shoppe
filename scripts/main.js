e90ca5419d4ee854b781d40358afefce2494da31
Wesley <wesleyadwebster@gmail.com>
2023-06-17 00:55:16 -0500

    Changed the font of body text. Cleaned up carousel buttons

--- scripts/main.js
+++ scripts/main.js
@@ -2,26 +2,58 @@ const carouselImageOne = 'assets/carousel-images/carousel-image-1.jpg';
 const carouselImageTwo = 'assets/carousel-images/carousel-image-2.jpg';
 const carouselImageThree = 'assets/carousel-images/carousel-image-3.jpg';
 
-const carouselImages = [carouselImageOne, carouselImageTwo, carouselImageThree];
+const carouselItems = [
+    {
+        img: carouselImageOne,
+        text: `
+            Indulge in a sensory symphony that will transport you to frozen bliss. Our luscious ice cream, carefully crafted 
+            and swirled into convenient buckets, promises an unforgettable journey of flavor. Each scoop is a revelation, 
+            an escape from the mundane, as velvety textures dance with tantalizing tastes, and vibrant colors tantalize 
+            your eyes. From the first spoonful to the last lingering lick, our ice cream unveils a medley of delectable 
+            ingredients, harmoniously blended to create an epicurean masterpiece. Lose yourself in a sea of creamy decadence, 
+            where dreams of dessert are scooped into reality. Dive into a bucketful of pure delight, and savor every moment of 
+            this frozen euphoria.
+        `
+    },
+    {
+        img: carouselImageTwo,
+        text: `
+            Behold a picturesque vision that will awaken your taste buds and ignite your imagination. A velvety mound of 
+            vanilla ice cream, smooth and tantalizingly creamy, sits nestled in a glistening porcelain bowl. Its pristine 
+            white canvas is a testament to purity and simplicity, offering the perfect backdrop for an artful masterpiece.
+        `
+    },
+    {
+        img: carouselImageThree,
+        text: `
+            Prepare to be captivated by a masterful presentation of chocolate ecstasy. Three scoops of velvety, rich 
+            chocolate ice cream are meticulously arranged, forming an enticing symphony of flavor and visual delight.
+        `
+    }
+];
 
-console.log(carouselImages);
+console.log(carouselItems);
 
 // Get necessary elements
 const carouselImgElement = document.getElementById("carousel-img");
+const carouselTextElement = document.getElementById("carousel-text");
 const prevBtn = document.getElementById("prev-btn");
 const nextBtn = document.getElementById("next-btn");
 
+// image index
 let currentImageIndex = 0;
 
 // Function to update the image and buttons
-function updateCarousel() {
-    carouselImgElement.src = carouselImages[currentImageIndex];
+const updateCarousel = () => {
+    // targets the actual html elements of src and text, and then sets it to the object key value pair
+    carouselImgElement.src = carouselItems[currentImageIndex].img;
+    carouselTextElement.textContent = carouselItems[currentImageIndex].text;
 
     prevBtn.disabled = currentImageIndex === 0;
-    nextBtn.disabled = currentImageIndex === carouselImages.length - 1;
-}
+    nextBtn.disabled = currentImageIndex === carouselItems.length - 1;
+};
 
-// Set initial image
+// Set initial image to render
 updateCarousel();
 
 // Set up event listeners for buttons
@@ -34,3 +66,24 @@ nextBtn.addEventListener("click", function () {
     currentImageIndex++;
     updateCarousel();
 });
+
+/*NOTES
+comparison operations like currentImageIndex === 0 or 
+currentImageIndex === carouselItems.length - 1 yield boolean values - either true or false.
+
+The === operator tests for strict equality, meaning it checks if the operands are of the same type 
+and have the same value.
+So in this case, prevBtn.disabled = currentImageIndex === 0; is assigning a boolean value 
+to the disabled property of the prevBtn object.
+
+If currentImageIndex is 0, currentImageIndex === 0 will be true and so prevBtn.disabled will be set to true, 
+disabling the button.
+
+If currentImageIndex is anything other than 0, currentImageIndex === 0 will be false and so prevBtn.disabled 
+will be set to false, enabling the button.
+
+The same principle applies to nextBtn.disabled = currentImageIndex === carouselItems.length - 1;.
+
+It's worth noting that the disabled property of a button is a boolean property, so assigning it a boolean value 
+is the correct approach.
+*/
